a7df0178f2a1b25d873a4d5daf0f4dd8f1241cf3
rajariandhana <rajariandhana@gmail.com>
2024-05-22 19:12:12 +0700

    need winning cond prolly

--- controller.js
+++ controller.js
@@ -2,39 +2,78 @@ const size = 10;
 const n_bombs = 10;
 const emo_bomb = '💣'
 const emo_flag = '⛳️'
+let toReveal = (size*size) - n_bombs;
+console.log(toReveal);
 let movR=[-1,-1,-1,0,0,1,1,1];
 let movC=[-1,0,1,-1,1,-1,0,1];
+let isPlaying=false;
 function Click(event)
 {
-    let evid = event.target.id;
-    let r = parseInt(evid.charAt(0),10);
-    let c = parseInt(evid.charAt(1),10);
-    let cell = document.getElementById(evid);
+    if(!isPlaying) return;
+    let cell = event.target;
+
+    let r = parseInt(cell.id.charAt(0),10);
+    let c = parseInt(cell.id.charAt(1),10);
     
     if(event.button===0) //left click
     {
+        if(revealed[r][c]) return;
         // event.target.style.backgroundColor = "red";
         if(myGrid[r][c]==0)
         {
             BFS(r,c);
+        }
+        else if(myGrid[r][c]==emo_bomb)
+        {
+            // GameOver
+            RevealBombs();
+            Win(false);
+        }
+        else if(NotZeroNotBomb(r,c))
+        {
+            // RevealCell(r,c);
+            let cell = GetCell(r,c);
+            BeigeCell(cell,myGrid[r][c]);
+        }
+        console.log(toReveal);
+        if(toReveal<=0)
+        {
+            Win(true);
         }
     }
     else if(event.button === 2) //right click
     {
         // event.target.style.backgroundColor = "blue";
         Flag(cell,r,c);
-
     }
 }
 
 function Flag(cell,r,c)
 {
-    if(cell.textContent==emo_flag)
+    //must use text content, otherwise myGrid overwritten
+    if(!revealed[r][c])
     {
-        cell.textContent=myGrid[r][c];
+        if(cell.textContent==emo_flag)
+        {
+            cell.textContent='';
+        }
+        else
+        {
+            cell.textContent=emo_flag;
+        }
+    }
+    
+}
+function Win(win)
+{
+    let mes = document.getElementById('message');
+    if(win)
+    {
+        mes.textContent = 'Congrats';
     }
     else
     {
-        cell.textContent=emo_flag;
+        mes.textContent = ':(';
     }
 }
+isPlaying = true;
--- grid.js
+++ grid.js
@@ -1,28 +1,38 @@
 let gridBody = document.getElementById('gridBody');
 
 let myGrid = [];
+let revealed = [];
 let bombs = [];
 let colors = ['black','blue','green','red','purple','orange','aqua','']
 function GenerateGrid()
 {
+    let flip=true;
     for(let r=0; r<size; r++)
     {
         let newRow = document.createElement('tr');
         let rowList = [];
+        let revList = [];
         for(let c=0; c<size; c++)
         {
             let cell = document.createElement('td');
             cell.id = r.toString()+c.toString();
-            cell.textContent = 0;
+            // cell.textContent = 0;
             cell.addEventListener('mousedown',Click);
             cell.addEventListener('contextmenu',function(event){
                 event.preventDefault();
             });
             newRow.appendChild(cell);
             rowList.push(0);
+            revList.push(false);
+
+            if(flip) cell.className = 'tileGreenLight';
+            else cell.className = 'tileGreenDark';
+            flip = !flip;
         }
+        flip = !flip;
         gridBody.appendChild(newRow);
         myGrid.push(rowList);
+        revealed.push(revList);
     }
     console.log(myGrid);
 }
@@ -59,7 +69,7 @@ function UpdateGrid()
         for(let c=0; c<size; c++)
         {
             let cell = GetCell(r,c);
-            cell.textContent = myGrid[r][c];
+            // cell.textContent = myGrid[r][c];
             cell.style.color = myGrid[r][c]==emo_bomb? 'black':colors[myGrid[r][c]];
         }
     }
@@ -87,26 +97,37 @@ function BFS(r,c)
     let q = new Queue();
     q.enqueue([r,c]);
     vis[r][c] = true;
+
     while(!q.isEmpty())
     {
         let cur = q.dequeue();
         let cr = cur[0];
         let cc = cur[1];
-        if(myGrid[cr][cc]!=0) continue;
         let cell = GetCell(cr,cc);
-        cell.textContent = 'VIS';
+        
+        if(myGrid[cr][cc]==emo_bomb) continue;
+        if(NotZeroNotBomb(cr,cc))
+        {
+            BeigeCell(cell,myGrid[cr][cc]);
+            continue;
+        }
+        else
+        {
+            BeigeCell(cell,null);
+        }
         for(let i=0;i<8;i++)
         {
             let nr = cr+movR[i];
             let nc = cc+movC[i];
             if(!CellValid(nr,nc)) continue;
             if(vis[nr][nc]) continue;
-            if(myGrid[nr][nc]!=0) continue;
+            if(myGrid[nr][nc]==emo_bomb) continue;
             q.enqueue([nr,nc]);
-            console.log("push "+nr+" "+nc);
+            // console.log("push "+nr+" "+nc);
             vis[nr][nc]=true;
         }
     }
+    console.log(myGrid);
 
 }
 function GetCell(r,c)
@@ -115,6 +136,50 @@ function GetCell(r,c)
     let cellID = r.toString()+c.toString();
     let cell = document.getElementById(cellID);
     return cell;
+}
+function NotZeroNotBomb(r,c)
+{
+    if(myGrid[r][c]==0 || myGrid[r][c]==emo_bomb)return false;
+    return true;
+}
+function BeigeCell(cell,word)
+{
+    revealed[parseInt(cell.id.charAt(0),10)][parseInt(cell.id.charAt(1),10)]=true;
+    toReveal -= 1;
+    if(word) cell.textContent = word;
+    if(cell.className=='tileGreenLight') cell.className = 'tileBeigeLight';
+    else if(cell.className=='tileGreenDark') cell.className = 'tileBeigeDark';
+}
+
+function RevealCell(r,c)
+{
+    let cell = GetCell(r,c);
+    if(myGrid[r][c]==0)
+    {
+        // cell.textContent = "REV";
+        if(cell.className=='tileGreenLight') cell.className = 'tileBeigeLight';
+        else if(cell.className=='tileGreenDark') cell.className = 'tileBeigeDark';
+        // myGrid[r][c]='REV';
+    }
+    else
+    {
+        cell.textContent = myGrid[r][c];
+    }
+    // else if(myGrid[r][c]==emo_bomb)
+    // {
+    //     cell.textContent = emo_bomb;
+    // }
+}
+function RevealBombs()
+{
+    for(let i=0; i<bombs.length; i++)
+    {
+        let r = bombs[i][0];
+        let c = bombs[i][1];
+        revealed[r][c]=true;
+        let cell = GetCell(r,c);
+        cell.textContent = myGrid[r][c];
+    }
 }
 
 GenerateGrid();
